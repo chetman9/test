7fdf6f1271cf3cdf127a41e8a18d000b1c016df0
kostya <kostyadryagin@gmail.com>
2021-10-24 22:54:05 +0600

    mailer

---
+++ .history/app/js/main_20211024162653.js
@@ -0,0 +1,69 @@
+$(function () {
+    
+    $('.slider').slick({
+        infinite: true,
+        slidesToShow: 3,
+        slidesToScroll: 1,
+        dots: true,
+        prevArrow: $('.control__prev'),
+        nextArrow: $('.control__next')
+      });
+
+      $('#form').validate({
+        rules: {
+          name: {
+            required: true,
+            minlength: 2,
+            maxlength: 50,
+            number: false,
+            checkallowedchars: true
+          },
+          phone: "required",
+          yes:"required"
+        },
+        
+        messages : {
+          name: "Введите ваше имя",
+          phone: "Введите ваш телефон",
+          yes: "Подтвердите",
+        },
+
+
+      /*submitHandler: function(form, event) { 
+        $('.btm_form').on('click', function (){
+          $('.popup-show').fadeIn();
+        });
+        return false; 
+    }*/
+
+      });
+
+      $.validator.addMethod("checkallowedchars", function (value) {
+        var pattern = new RegExp('^[ а-я]+', 'g');
+        return /^[ а-я]+$/i.test(value);
+    }, "The field contains non-admitted characters")
+
+    
+      $('input[name=phone]').mask("+7 (999) 999-99-99");
+
+      $('.popup__close').on('click', function (){
+        $('.popup').fadeOut();
+      });
+
+      $('form').submit(function(e) {
+        e.preventDefault();
+        $.ajax({
+            type: "POST",
+            url: "mailer/smart.php",
+            data: $(this).serialize()
+        }).done(function() {
+            $(this).find("input").val("");
+            $('#consultation, #order').fadeOut();
+            $('.overlay, #thanks').fadeIn('slow');
+
+            $('form').trigger('reset');
+        });
+        return false;
+    });
+
+});
